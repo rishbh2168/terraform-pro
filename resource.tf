7347b4436053c7828eee09f6d1ae5156ac40328a
rishbh2168 <rishabhrgupta2168@gmail.com>
2025-07-17 04:46:59 +0000

    third

--- resource.tf
+++ resource.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "terraazure" {
-  name     = "terraazure"
+resource "azurerm_resource_group" "terraazure9026" {
+  name     = "terraazure9026"
   location = "East US"
 }
